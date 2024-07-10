883534f60daf8a3db368e8801b8d09c2803df393
Evan Summers <evan.summers@wandelbots.com>
2024-07-10 18:27:41 +0200

    fix: Align client creation function with README

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
+import { NovaClient, NovaClientConfig } from "./NovaClient"
 export * from "./NovaClient"
 export * from "@wandelbots/wandelbots-api-client"
+
+export function createNovaClient(config: NovaClientConfig) {
+  return new NovaClient(config)
+}
--- test/programs.test.ts
+++ test/programs.test.ts
@@ -1,15 +1,14 @@
 /// <reference types="vite/client" />
 
 import { expect, test } from "vitest"
-import { NovaClient } from "../src"
+import { createNovaClient } from "../src"
 
 test("running a Wandelscript program", async () => {
-  const nova = new NovaClient({
+  const nova = createNovaClient({
     instanceUrl: import.meta.env.NOVA_INSTANCE_URL,
     cellId: "cell",
   })
 
   const devices = await nova.api.deviceConfig.listDevices()
-  console.log(devices)
   expect(devices.length).toBeGreaterThan(0)
 })
