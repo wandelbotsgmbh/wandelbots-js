b5133a9285bcf02c8cfb8ec09230b6896407a3ab
Evan Summers <evan.summers@wandelbots.com>
2024-09-05 09:10:29 +0200

    test: Added test for jogging opt switch regression

--- test/jogging.test.ts
+++ test/jogging.test.ts
@@ -1,10 +1,28 @@
 /// <reference types="vite/client" />
 
-import { test } from "vitest"
+import { expect, test } from "vitest"
 import { NovaClient } from "../dist"
 
-test("things compile and initialize", async () => {
+test("jogging", async () => {
   const nova = new NovaClient({
     instanceUrl: "https://mock.example.com",
   })
+
+  const jogger = await nova.connectJogger("0@mock-ur5e")
+
+  jogger.setJoggingMode("cartesian", {
+    tcpId: "tcp",
+    coordSystemId: "world",
+  })
+
+  const oldWebsocket = jogger.activeWebsocket
+
+  // Test that jogger reopens websocket when cartesian opts change
+  jogger.setJoggingMode("cartesian", {
+    tcpId: "tcp",
+    coordSystemId: "base",
+  })
+
+  const newWebsocket = jogger.activeWebsocket
+  expect(newWebsocket).not.toBe(oldWebsocket)
 })
